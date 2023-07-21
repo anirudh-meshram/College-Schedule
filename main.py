50f4c8764237608f3ec86c07cb36c7c5632ec074
anirudh-meshram <rxydenxd@outlook.com>
2023-07-21 12:03:06 +0530

    request_url now references requestURL.txt in the project root directory; Other minor changes

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from tabulate import tabulate as table
 from datetime import date as dt
 
 # Request URL gathered from Network data at time-table.sicsr.ac.in; Manipulating the string according to today's date
-request_url = 'http://time-table.sicsr.ac.in/report.php?from_day={from_day}&from_month={from_month}&from_year={from_year}&to_day={to_day}&to_month={to_month}&to_year={to_year}&areamatch=&roommatch=&typematch%5B%5D=P&typematch%5B%5D=R&namematch=&descrmatch=&creatormatch=&match_confirmed=2&output=0&output_format=1&sortby=s&sumby=d&phase=2&datatable=1'.format(from_day = str(int(dt.today().strftime('%d'))), from_month = str(int(dt.today().strftime('%m'))), from_year = str(int(dt.today().strftime('%Y'))), to_day = str(int(dt.today().strftime('%d'))), to_month = str(int(dt.today().strftime('%m'))), to_year = str(int(dt.today().strftime('%Y'))))
+with open('./requestURL.txt','r') as url_file:
+    request_url = url_file.read()
+
+request_url = request_url.format(from_day = str(int(dt.today().strftime('%d'))), from_month = str(int(dt.today().strftime('%m'))), from_year = str(int(dt.today().strftime('%Y'))), to_day = str(int(dt.today().strftime('%d'))), to_month = str(int(dt.today().strftime('%m'))), to_year = str(int(dt.today().strftime('%Y'))))
 
 try: results = requests.get(request_url,timeout=5)                                 # Making the HTTP request
 except ConnectionError:
@@ -28,20 +31,20 @@ def process_data(txt):
     for i in lecture:
         details.append(i.split(','))                                # Splitting the list for only importing certain data
     for lecture in details:
-        dataset.append([lecture[0], lecture[2], lecture[3][0:8], lecture[4][0:8], int(lecture[5][0])])
+        dataset.append([lecture[0][:lecture[0].rindex('-')], lecture[2], lecture[3][0:8], lecture[4][0:8], int(lecture[5][0])])
     return dataset                                                  # Return the processed data for displaying information
 
 def display_results(dataset):
-    if len(dataset)==0:
+    if len(dataset)==0:                                             # If no lectures are there today
         print("There are no lectures today.")
         exit()
-    headers = ["Lecture Name", "Room No.", "Start Time", "End Time", "Duration (in hrs)"]
     noOfLectures = len(dataset)                                     # For displaying the total number of lectures
-    noOfHours = 0
+    noOfHours = 0                                                   # Storing the number of hours
     for i in dataset: noOfHours += i[-1]                            # Calculating the total numbers of hours
-    print("Today is:", dt.today().strftime('%d-%m-%Y'))
+    print("Today is:", dt.today().strftime('%d-%m-%Y'))             # Displaying the date in DD-MM-YYYY format
     print("There are {} lectures today".format(noOfLectures))       # No. of Lectures
     print("You have to attend {} hours today".format(noOfHours))    # No. of Hours
-    print(table(dataset, headers = headers, tablefmt='grid'))       # Printing the information in a table
+    print("Your college day starts from {} and ends at {}".format(dataset[0][2], dataset[-1][3]))
+    print(table(dataset, headers = ["Lecture Name", "Room No.", "Start Time", "End Time", "Duration (in hrs)"], tablefmt='grid'))       # Printing the information in a table
 
 display_results(process_data(results.text))
