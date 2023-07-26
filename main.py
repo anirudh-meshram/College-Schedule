860d89f8bde41d1c297879c40e247c7d38d7e40b
anirudh-meshram <rxydenxd@outlook.com>
2023-07-27 02:16:09 +0530

    Moved everything to main.py

--- main.py
+++ main.py
@@ -2,46 +2,61 @@
 import os
 import sys
 import subprocess
+from datetime import date as dt, timedelta
+from datetime import datetime
+from time import sleep
 
-def update():
+def generateData():
     try:
-        subprocess.run(['python3', 'update.py'], check=True)
+        print('\033[35mLoading...\n\033[0m')
+        page = requests.get('http://time-table.sicsr.ac.in/day.php?year={}&month={}&day={}&area=1&room=29'.format(datetime.now().year, datetime.now().month if str(datetime.now().month).startswith('0') else str('0'+str(datetime.now().month)), datetime.now().day),timeout=5).text
     except:
-        print('\033[31mUnable to run update.py\033[0m')
+        print('\033[31mERROR: Connection failed! Please try again!\033[0m')
         sys.exit()
+    soup = bs(page, 'html.parser')
+    options = soup.find(id='type').prettify().split('\n')
+    for i in options:
+        if i.strip() == '' or i.strip() == '</option>' or i.strip() == '<select id="type" name="type">':
+            options.pop(options.index(i))
+    for i in range(len(options)):
+        options[i] = options[i].strip()
+    options.pop()
+    types = {}
+    for i in range(len(options)):
+        if options[i].startswith('<'):
+            types[list(types.keys())[-1]+1 if len(types)>= 1 else 0] = [options[i][options[i].index('"')+1], options[i+1]]
+    dataset = []
+    for i in types:
+        dataset.append([i, types[i][-1]])
+    return (types, dataset)
 
 def generateConfig():
-    try:
-        subprocess.run(['python3', 'match_type.py'], check=True)
-    except:
-        print('\033[31mUnable to run match_type.py\033[0m')
-        sys.exit()
-
-def ensure_dependencies():
-    # Looks for '.ensure_dependencies' in the root folder
-    if not '.ensure_dependencies' in os.listdir():
-        print('\033[35m.ensure_dependencies not found in the root directory. Installing dependencies... (Only for the first execution)\033[0m')
-        with open('.ensure_dependencies','w+', encoding = 'utf-8') as file: file.write('True')
-        ensure_pip()
-        dependencies = ['requests', 'tabulate', 'bs4']
-        for package in dependencies:
-            try: subprocess.check_call(["pip","show",package])
-            except subprocess.CalledProcessError:
-                print(f"\033[33m{package} is not installed. Installing...\033[0m")
-                try:
-                    subprocess.check_call(["pip", "install", package])
-                    print(f"\033[32m{package} installed successfully.\033[0m")
-                except: print(f"\033[31mFailed to install {package}\033[0m")
-            except FileNotFoundError:
-                print('\033[31mERROR: pip not found or not reachable!\033[0m')
-                print('\033[31mPlease make sure the following modules are installed using pip:\033[0m')
-                print(*dependencies)
-                sys.exit()
-        print('--------------------------')
-        print('\033[32mDependencies are installed\033[0m')
-        print('--------------------------')
+    types, dataset = generateData()
+    print(table(dataset, headers = ["S. No", "Subject"], tablefmt='simple'))       # Printing the information in a table
+    print('\n\033[36m( Scroll up to see the entire list )\n\033[0m')
+    while True:
+        choice = input('\nEnter your choices separated by commas\nFor example: 0,2\tOR\t 1,3,5\n\n> ')
+        choice = choice.split(',') 
+        try:
+            print('\nYou have chosen:\n')
+            for i in choice:
+                print(types[int(i)][-1])
+        except (KeyError, ValueError):
+            print('\033[31mInvalid input!\033[0m')
+            print('\033[33mLoading your choices again...\033[0m')
+            sleep(1.5)
+            continue
+        confirm = input('\n\033[32mConfirm? (Enter "y" to confirm, any other key to choose again)\n\n> \033[0m')
+        if confirm != 'y' and confirm != 'Y': continue
+        break
+    with open('.config', 'w+', encoding='utf-8') as config:
+        for i in choice:
+            config.write(types[int(i)][0])
         os.system('cls') if os.name == 'nt' else os.system('clear')
-                
+        print('\033[32mSuccessfully generated .config file\033[0m')
+        print('\n\033[33mIMPORTANT: If you want to reset your configuration, Remove the file ".config" from your project directory\n\033[0m')
+        input('\033[32mPress Enter to continue\033[0m')
+        os.system('cls') if os.name == 'nt' else os.system('clear')
 def ensure_pip():
     try:
         import pip
@@ -73,8 +88,91 @@ def ensure_pip():
             sys.exit()
     print("\033[32mpip is now installed\033[0m")
     return True
-            
+
+def ensure_dependencies():
+    # Looks for '.ensure_dependencies' in the root folder
+    if not '.ensure_dependencies' in os.listdir():
+        print('\033[35m.ensure_dependencies not found in the root directory. Installing dependencies... (Only for the first execution)\033[0m')
+        with open('.ensure_dependencies','w+', encoding = 'utf-8') as file: file.write('True')
+        ensure_pip()
+        dependencies = ['requests', 'tabulate', 'bs4']
+        for package in dependencies:
+            try: subprocess.check_call(["pip","show",package])
+            except subprocess.CalledProcessError:
+                print(f"\033[33m{package} is not installed. Installing...\033[0m")
+                try:
+                    subprocess.check_call(["pip", "install", package])
+                    print(f"\033[32m{package} installed successfully.\033[0m")
+                except: print(f"\033[31mFailed to install {package}\033[0m")
+            except FileNotFoundError:
+                print('\033[31mERROR: pip not found or not reachable!\033[0m')
+                print('\033[31mPlease make sure the following modules are installed using pip:\033[0m')
+                print(*dependencies)
+                sys.exit()
+        print('--------------------------')
+        print('\033[32mDependencies are installed\033[0m')
+        print('--------------------------')
+        os.system('cls') if os.name == 'nt' else os.system('clear')
+
 ensure_dependencies() # Ensure dependencies
+
+import requests
+from requests.exceptions import ConnectionError
+from tabulate import tabulate as table
+from bs4 import BeautifulSoup as bs
+
+def getGithubContent(url):
+    headers = {'Accept': 'application/vnd.github.v3.raw'}
+    try:
+        response = requests.get(url, headers=headers)
+    except:
+        print('\033[33mUnable to access remote content, Skipping update...\033[0m')
+        sys.exit()
+    if response.ok: return response.json()
+    else: return False
+
+def update(response):
+    if response:
+        print('\033[32mChecking for updates...\033[0m')
+
+        # Deleting files which are not in remote directory
+        files = [i['name'] for i in response if i['type']=='file' and i['name'][0] != '.']
+        for i in os.listdir():
+            if not os.path.isdir(i):
+                if not i in files:
+                    os.remove(i)
+                    print(f"\033[32mSucessfully deleted {i}\033[0m")
+
+        # Downloading missing files
+        for i in response:
+            if i['type'] == 'file' and i['name'][0] != '.':
+                if not i['name'] in os.listdir():
+                    try:
+                        remote = requests.get(i['download_url']).text
+                    except:
+                        print('\033[31mUnable to access remote content, Skipping update...\033[0m')
+                        return
+                    with open(i['name'], 'w', encoding = 'utf-8') as file:
+                        file.write(remote)
+                        print(f"\033[32mSuccesfully downloaded {i['name']}\033[0m")
+
+        # Updating the existing content (except for other folders, and dot-files)
+        for i in response:
+            if i['type'] == 'file' and i['name'][0] != '.':
+                try:
+                    remote = requests.get(i['download_url']).text
+                except:
+                    print('\033[31mUnable to access remote content, Skipping update...\033[0m')
+                    return
+                with open(i['name'], 'r', encoding = 'utf-8') as this:
+                    local = this.read()
+                if remote != local:
+                    with open(i['name'], 'w', encoding = 'utf-8') as file:
+                        file.write(remote)
+                        print(f"\033[32mSuccessfully updated {i['name']}\033[0m")
+    else:
+        print('\033[31mUnable to access remote content, Skipping update...\033[0m')
+        return
 
 def process_data(txt):
     dataset = []
@@ -104,29 +202,10 @@ def display_results(dataset):
     print(table(dataset, headers = ["Lecture Name", "Room No.", "Start Time", "End Time", "Duration (in hrs)"], tablefmt='grid'))       # Printing the information in a table
     print('\n\033[33m(If the table is not properly visible, Kindly zoom out till the table fits the terminal window ( Ctrl + - ))\033[0m')
 
-if not 'update.py' in os.listdir():
-    print('\033[31mERROR: update.py missing in root directory. Kindly download the latest project files from:\033[0m')
-    print('https://github.com/anirudh-meshram/College-Schedule')
-else:
-    update()
-
-if not 'match_type.py' in os.listdir():
-    print('\033[31mmatch_type.py missing in root directory. Kindly download the latest project files from:\033[0m')
-    print('https://github.com/anirudh-meshram/College-Schedule')
+update(getGithubContent('https://api.github.com/repos/anirudh-meshram/College-Schedule/contents/'))
 
 if not '.config' in os.listdir():
     generateConfig()
-
-# Using requests to make HTTP requests
-import requests
-from requests.exceptions import ConnectionError
-
-# For displaying the information in a table
-from tabulate import tabulate as table
-
-# For import current data in order to get today's time-table from the website
-from datetime import date as dt, timedelta
-from datetime import datetime
 
 def getNextDate():
     today = dt.today()
